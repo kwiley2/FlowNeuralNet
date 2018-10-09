d1e2fc2e57a47b15f04f60969f9ca82a270e43f3
Keith Wiley <kwiley@sas.upenn.edu>
2018-10-09 17:40:47 -0400

    Added script to create synch vs coupling plot

---
+++ EnsembleAvgScript.m
@@ -0,0 +1,17 @@
+%Average over many (random) topologies
+n_trials = 10;
+n_couplings = 10;
+ensemble_avg_order_params = zeros(n_couplings,1);
+ensemble_std_dev_order_params = zeros(n_couplings,1);
+for j=1:n_couplings
+    K_coupling = j/2;
+
+    avg_order_param = zeros(n_trials,1);
+    for i=1:n_trials
+        avg_order_param(i) = FlowDiffusionNeuralSim(K_coupling);
+    end
+    ensemble_avg_order_params(j) = mean(avg_order_param);
+    ensemble_std_dev_order_params(j) = std(avg_order_param);
+end
+plot(ensemble_avg_order_params);
+%ensemble_std_dev_order_param
